5d31a935cfcc50d5632818fec80a45412b46c20a
Atakan Arslan <atak.arslan@hotmail.com>
2024-01-31 19:17:52 +0300

    Spring Boot Practices

---
+++ DataJPA-PostgreSQL/src/main/java/com/example/DataJPAPostgreSQL/DataJpaPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataJPAPostgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaPostgreSqlApplication.class, args);
+	}
+
+}
---
+++ DataJPA-PostgreSQL/src/test/java/com/example/DataJPAPostgreSQL/DataJpaPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataJPAPostgreSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
