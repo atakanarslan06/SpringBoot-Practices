c509df496853cd53109208947995321bbc7186e6
Atakan Arslan <atak.arslan@hotmail.com>
2024-02-01 17:51:23 +0300

    SwaggerApiDoc

---
+++ ApiDoc/src/main/java/com/springexamp/apidoc/PetDto.java
@@ -0,0 +1,18 @@
+package com.springexamp.apidoc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PetDto {
+    private int id;
+    private String name;
+    private Date date;
+}
