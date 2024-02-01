b795bfc35c40bfee00f3dbfddecd550f91eda427
Atakan Arslan <atak.arslan@hotmail.com>
2024-02-01 18:09:38 +0300

    Added Swagger Configuration

--- ApiDoc/src/main/java/com/springexamp/apidoc/PetDto.java
+++ ApiDoc/src/main/java/com/springexamp/apidoc/PetDto.java
@@ -1,5 +1,7 @@
 package com.springexamp.apidoc;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,8 +13,12 @@ import java.util.Date;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(value = "Benim pet nesnem",description = "Kara Baş")
 public class PetDto {
+    @ApiModelProperty(value = "Pet nesnesinin tekil Id alani")
     private int id;
+    @ApiModelProperty(value = "Pet nesnesinin adi alani")
     private String name;
+    @ApiModelProperty(value = "Pet nesnesinin  tarih alani")
     private Date date;
 }
