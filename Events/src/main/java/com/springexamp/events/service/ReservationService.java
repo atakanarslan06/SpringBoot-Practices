39d4bd9a1be56f687f12699c3bb8201f709f520a
Atakan Arslan <atak.arslan@hotmail.com>
2024-02-03 12:37:26 +0300

    Events

--- Events/src/main/java/com/springexamp/events/service/ReservationService.java
+++ Events/src/main/java/com/springexamp/events/service/ReservationService.java
@@ -4,12 +4,14 @@ import com.springexamp.events.api.BookingApi;
 import com.springexamp.events.event.ReservationCreatedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
     @Autowired
     ApplicationEventPublisher applicationEventPublisher;
+    @Async
     public void publishReservationEvent(BookingApi.HotelBookRequest hotelBookRequest){
         applicationEventPublisher.publishEvent(new ReservationCreatedEvent(hotelBookRequest));
     }
