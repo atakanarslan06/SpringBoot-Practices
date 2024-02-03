e2e1e85486c40f205b39108b350840326753c635
Atakan Arslan <atak.arslan@hotmail.com>
2024-02-03 10:58:34 +0300

    Pagination

--- Pagination/src/main/java/com/springexamp/pagination/api/BookApi.java
+++ Pagination/src/main/java/com/springexamp/pagination/api/BookApi.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +27,10 @@ public class BookApi {
     }
     @GetMapping("/p")
     public Page<Book> pagination(Pageable pageable){
+        return bookRepository.findAll(pageable);
+    }
+    @GetMapping("/s")
+    public Slice<Book> slice(Pageable pageable){
         return bookRepository.findAll(pageable);
     }
 }
